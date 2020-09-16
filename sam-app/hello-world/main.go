debd87c04a0011887c1bb6de8c7b2c1da911fcb9
shinnosuke-K <shin.050195@gmail.com>
2020-09-16 20:18:40 +0900

    delete: unnecessary line

--- sam-app/hello-world/main.go
+++ sam-app/hello-world/main.go
@@ -1,65 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"time"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"hello-world/model"
+	"hello-world/table"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type model struct {
-	DB *gorm.DB
-}
-
-type Content struct {
-	ID        int        `json:"id"`
-	TicketID  int        `json:"ticket_id"`
-	CreatedAt *time.Time `json:"created_at"`
-	UpdatedAt *time.Time `json:"updated_at"`
-}
-
-type Response struct {
-	ID int `json:"id"`
-}
-
-func createConnect() string {
-	USER := "root"
-	PASS := "root"
-	PROTOCOL := "tcp(zendesk-db:3306)"
-	DBNAME := "zendesk"
-	OPTION := "charset=utf8mb4&parseTime=True&loc=Local"
-
-	return fmt.Sprintf("%s:%s@%s/%s?%s", USER, PASS, PROTOCOL, DBNAME, OPTION)
-}
-
-func (db *model) open() error {
-	var err error
-	db.DB, err = gorm.Open(mysql.Open(createConnect()), nil)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (db *model) hasTable() bool {
-	return db.DB.Table("ticket_contents").Migrator().HasTable(&Content{})
-}
-
 func httpHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	var db model
-	if err := db.open(); err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
+	db := model.New()
 
-	fmt.Println(db.hasTable())
-	if !db.hasTable() {
-		err := db.DB.Migrator().CreateTable(&Content{})
+	has, err := db.HasTable(table.Ticket{})
+	if !has && err == nil {
+		err := db.DB.Migrator().CreateTable(&table.Ticket{})
 		if err != nil {
 			return events.APIGatewayProxyResponse{Body: err.Error()}, err
 		}
