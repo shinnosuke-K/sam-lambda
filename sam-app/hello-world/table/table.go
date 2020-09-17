4cd68d1a94d149aad5206f9595337388351eefa6
shinnosuke-K <shin.050195@gmail.com>
2020-09-17 18:56:42 +0900

    add: new

--- sam-app/hello-world/table/table.go
+++ sam-app/hello-world/table/table.go
@@ -5,13 +5,11 @@ import "gorm.io/gorm"
 type Table interface {
 	HasTable(db *gorm.DB) bool
 	CreateTable(db *gorm.DB) error
+	GetBody() []byte
+	Mapping(jsonBody []byte) error
 	Insert(db *gorm.DB)
 }
 
-func HasTable(t Table, db *gorm.DB) bool {
-	return t.HasTable(db)
-}
-
-func CreateTable(t Table, db *gorm.DB) error {
-	return t.CreateTable(db)
+func New() []Table {
+	return []Table{NewTicket(), NewUser(), NewCompany()}
 }
