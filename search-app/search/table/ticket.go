87df268be9e649507f01627abdd4e130098e8b4e
shinnosuke-K <shin.050195@gmail.com>
2020-09-25 00:42:39 +0900

    delete

--- search-app/search/table/ticket.go
+++ search-app/search/table/ticket.go
@@ -1,28 +1,8 @@
 package table
 
 import (
-	"fmt"
 	"time"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-
-	"github.com/jinzhu/gorm"
 )
-
-func createConnect() string {
-	USER := "root"
-	PASS := "root"
-	PROTOCOL := "tcp(zendesk-db:3306)"
-	DBNAME := "zendesk"
-	OPTION := "charset=utf8mb4&parseTime=True&loc=Local"
-
-	return fmt.Sprintf("%s:%s@%s/%s?%s", USER, PASS, PROTOCOL, DBNAME, OPTION)
-}
-
-func dbOpen() (*gorm.DB, error) {
-	return gorm.Open(mysql.Open(createConnect()), nil)
-}
 
 type Ticket struct {
 	ID             int64
