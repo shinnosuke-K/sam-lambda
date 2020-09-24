989af5f4e02af37ee2aee29c6d4183cfb8f0bee4
shinnosuke-K <shin.050195@gmail.com>
2020-09-24 22:36:24 +0900

    add

---
+++ list-app/hello-world/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"hello-world/table"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func createConnect() string {
+	USER := "root"
+	PASS := "root"
+	PROTOCOL := "tcp(zendesk-db:3306)"
+	DBNAME := "zendesk"
+	OPTION := "charset=utf8mb4&parseTime=True&loc=Local"
+
+	return fmt.Sprintf("%s:%s@%s/%s?%s", USER, PASS, PROTOCOL, DBNAME, OPTION)
+}
+
+func dbOpen() (*gorm.DB, error) {
+	return gorm.Open(mysql.Open(createConnect()), nil)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	db, err := dbOpen()
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	tickets := make([]table.Ticket, 0)
+
+	if err := db.Find(&tickets).Error; err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	jsonTicket, err := json.Marshal(&tickets)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body:       string(jsonTicket),
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
