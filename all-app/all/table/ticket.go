2b0243301e8f0749792699252c25f1eedd47204f
shinnosuke-K <shin.050195@gmail.com>
2020-09-26 16:05:55 +0900

    add: subjectの内容

--- all-app/all/table/ticket.go
+++ all-app/all/table/ticket.go
@@ -54,6 +54,17 @@ func (t *JsonTicket) GetBody() []byte {
 	types := []string{"problem", "incident", "question", "task"}
 	priority := []string{"urgent", "high", "normal", "low"}
 	status := []string{"new", "open", "pending", "hold", "solved", "closed"}
+	subject := []string{
+		"【完全保存版】問い合わせの見積もりとスケジュール管理",
+		"問い合わせが驚くほど良くわかる、まとめ記事のまとめ",
+		"知らないヒトは絶対損してる！ 4円で遊べる『問い合わせガイド』",
+		"質問はエンジニアを幸せにはしなかった",
+		"質問割ろうぜ！ 2日6分で質問が手に入る「1分間質問運動」の動画が話題に",
+		"もう質問って絶対必要なんだから、3日10円出して「質問」で勉強しときなよ！",
+		"コピペで使えるオシャレなATOM見本 81 （全組み合わせ付） ",
+		"自分は探すものではなく、つくるもの。それが「ATOM」",
+		"ATOM次期システム関連のまとめ",
+	}
 
 	test := JsonTicket{}
 
@@ -63,9 +74,9 @@ func (t *JsonTicket) GetBody() []byte {
 			CreateTime:     time.Now().Add(time.Duration(n)),
 			UpdateTime:     time.Now().Add(time.Duration(n)),
 			Type:           types[n%4],
-			Subject:        "",
-			Priority:       priority[n%4],
-			Status:         status[n%4],
+			Subject:        subject[n%len(subject)],
+			Priority:       priority[n%len(priority)],
+			Status:         status[n%len(status)],
 			Tags:           []string{},
 			RequesterID:    int64(n % 3),
 			AssigneeID:     int64(n%3 + 1),
